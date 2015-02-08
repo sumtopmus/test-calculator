c63621aacece5e06f9928c479d0692981ede0918
sumtopmus <sumtopmus@users.noreply.github.com>
2015-02-08 16:13:51 -0500

    Lecture 2 - done

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    private var isTypingANumber: Bool = false
+    private var isTypingANumber = false
+    
+    private var operandStack = Array<Double>()
     
     @IBOutlet weak var display: UILabel!
+
     @IBAction func digit(sender: UIButton) {
         let digit = sender.currentTitle!
         if (!isTypingANumber) {
@@ -20,6 +23,57 @@ class ViewController: UIViewController {
             display.text = digit
         } else {
             display.text = display.text! + digit
+        }
+    }
+    
+    @IBAction func enter() {
+//		if isTypingANumber {
+			operandStack.append(displayValue)
+			isTypingANumber = false
+//		}
+		println(operandStack)
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+		if isTypingANumber {
+			enter()
+		}
+        let operation = sender.currentTitle!
+		switch operation {
+		case "+": performBinaryOperation{$1 + $0}
+		case "−": performBinaryOperation{$1 - $0}
+		case "×": performBinaryOperation{$1 * $0}
+		case "÷": performBinaryOperation{$1 / $0}
+		case "√": performUnaryOperation(sqrt)
+		default: println("Wrong operation!")
+		}
+	}
+
+	func performBinaryOperation(operation: (Double, Double) -> Double) {
+		if operandStack.count >= 2 {
+//			TODO: resolve strange behaviour
+//			isTypingANumber = true
+			displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+			enter()
+		}
+	}
+
+	func performUnaryOperation(operation: (Double) -> Double) {
+		if operandStack.count >= 1 {
+//			TODO: resolve strange behaviour
+//			isTypingANumber = true
+			displayValue = operation(operandStack.removeLast())
+			enter()
+		}
+	}
+
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+//            isTypingANumber = false
+            display.text = "\(newValue)"
         }
     }
 }
