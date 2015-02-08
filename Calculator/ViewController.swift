cafd4d44e9699f0b843188166825c46f58272539
sumtopmus <sumtopmus@users.noreply.github.com>
2015-02-08 00:51:20 -0500

    Lecture 1 - done

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    private var isTypingANumber: Bool = false
+    
+    @IBOutlet weak var display: UILabel!
+    @IBAction func digit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if (!isTypingANumber) {
+            isTypingANumber = true
+            display.text = digit
+        } else {
+            display.text = display.text! + digit
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
